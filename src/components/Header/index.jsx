358d9d0a9bd54a0ef337e072b04a077b3b51cc0a
myrojoylee <>
2024-03-03 20:14:43 -0500

    basic setup with header and footer created

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import '../../Styles/Header.css';
+
+function Header() {
+    return (
+        <header>
+            <h1>This is where the nav goes.</h1>
+        </header>
+    )
+}
+
+export default Header;
