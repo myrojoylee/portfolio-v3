8fd1d5864d6d9295c13057c30516e79195ad2bbb
myrojoylee <>
2024-03-10 22:57:09 -0400

    added basic card with animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Footer from './components/Footer';
 import Header from './components/Header';
+import MainContent from './components/MainContent';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <MainContent/>
       <Footer />
     </div>
   )
---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,11 @@
+import '../../Styles/Contact.css';
+
+function Contact() {
+    return (
+        <section className="contact card">
+            <h3>Contact me Here</h3>
+        </section>
+    )
+}
+
+export default Contact;
